9ff092161166a55f630f2565d4a9063b4fd31c9c
lutongling <lu.ton@northeastern.edu>
2021-02-08 22:20:46 -0500

    updated controller and template

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -4,7 +4,11 @@ var $passwordFld;
 var $firstNameFld;
 var $lastNameFld;
 var $roleFld;
+
 var $createBtn;
+var $updateBtn;
+
+var userService = new AdminUserServiceClient();
 
 var users = [];
 
@@ -22,54 +26,96 @@ function renderUsers(users) {
                 <td>${user.role}</td>
                 <td>
                     <button id="${i}" class="fa-2x fa fa-times wbdv-remove"></button>
-                    <button class="fa-2x fa fa-pencil wbdv-edit"></button>
+                    <button id="${user._id}" class="fa-2x fa fa-pencil wbdv-edit"></button>
                 </td>
             </tr>`)
     }
 
-    $(".wbdv-remove").click(deleteUser)
+    $(".wbdv-remove").click(deleteUser);
+    $(".wbdv-edit").click(selectUser);
 }
 
+var selectedUser = null;
 
-function createUser(user) {
-    users.push(user);
-    renderUsers(users);
+function selectUser(event) {
+    var selectBtn = $(event.target);
+    var id = selectBtn.attr("id");
+
+    selectedUser = users.find(user => user._id === id);
+
+    $usernameFld.val(selectedUser.username);
+    $passwordFld.val(selectedUser.password);
+    $firstNameFld.val(selectedUser.firstName);
+    $lastNameFld.val(selectedUser.lastName);
+    $roleFld.val(selectedUser.role);
 }
 
 function deleteUser(event) {
     var button = $(event.target);
-    var id = button.attr("id");
-    users.splice(id, 1);
-    renderUsers(users);
+    var index = button.attr("id");
+    var id = users[index]._id;
+
+    userService.deleteUser(id)
+        .then(function (status) {
+            users.splice(index, 1);
+            renderUsers(users);
+        })
+}
+
+function createUser() {
+    var newUser = {
+        username: $usernameFld.val(),
+        password: $passwordFld.val(),
+        firstName: $firstNameFld.val(),
+        lastName: $lastNameFld.val(),
+        role: $roleFld.val()
+    };
+
+    userService.createUser(newUser)
+        .then(function (actualUser) {
+            users.push(actualUser);
+            renderUsers(users);
+        })
+}
+
+
+function updateCourse() {
+    // console.log(selectedUser);
+    selectedUser.username = $usernameFld.val();
+    selectedUser.password = $passwordFld.val();
+    selectedUser.firstName = $firstNameFld.val();
+    selectedUser.lastName = $lastNameFld.val();
+    selectedUser.role = $roleFld.val();
+
+    userService.updateUser(selectedUser._id, selectedUser)
+        .then(function (status) {
+            var index = users.findIndex(user => user._id === selectedUser._id);
+            users[index] = selectedUser;
+            renderUsers(users);
+        })
 }
 
 function main() {
     $rowTemplate = $(".wbdv-tbody");
+
     $usernameFld = $("#usernameFld");
     $passwordFld = $("#passwordFld");
     $firstNameFld = $("#firstNameFld");
     $lastNameFld = $("#lastNameFld");
     $roleFld = $("#roleFld");
+
     $createBtn = $(".wbdv-create");
+    $createBtn.click(createUser);
 
-    $createBtn.click(function() {
-        createUser({
-            username: $usernameFld.val(),
-            password: $passwordFld.val(),
-            firstName: $firstNameFld.val(),
-            lastName: $lastNameFld.val(),
-            role: $roleFld.val()
+    $updateBtn = $(".wbdv-update");
+    $updateBtn.click(updateCourse);
+
+    userService.findAllUsers()
+        .then(function (actualUserFromServer) {
+            users = actualUserFromServer;
+            renderUsers(users);
         });
-
-        // clear the input field box after creating
-        $usernameFld.val("");
-        $passwordFld.val("");
-        $firstNameFld.val("");
-        $lastNameFld.val("");
-        $roleFld.val("");
-    });
-
 }
 
 // wait the whole DOM has loaded, then invoke this function
-jQuery(main);
+$(main);
