7463242d7349737ee5ad41f69ca7ccc195c8e9cd
lutongling <lu.ton@northeastern.edu>
2021-02-10 20:40:42 -0500

    updated the database url

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,11 +1,19 @@
+/**
+ * Old fashion way to create a "class" in JavaScript.
+ * This class represents the web service to interact with the server database.
+ * @constructor
+ */
 function AdminUserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/tongling/users';
+
+    // the remote server url
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001054187/users';
     var self = this;
 
+    // POST request
     function createUser(user) {
         return fetch(self.url, {
             method: 'POST',
@@ -14,6 +22,7 @@ function AdminUserServiceClient() {
         }).then(response => response.json())
     }
 
+    // GET request
     function findAllUsers() {
         return fetch(self.url)
             .then(function(response){
@@ -21,6 +30,7 @@ function AdminUserServiceClient() {
             })
     }
 
+    // PUT request
     function updateUser(userId, user) {
         return fetch(`${self.url}/${userId}`,{
             method: 'PUT',
@@ -29,6 +39,7 @@ function AdminUserServiceClient() {
         }).then(response => response.json())
     }
 
+    // DELETE request
     function deleteUser(userId) {
         return fetch(`${self.url}/${userId}`,
                      {method: 'DELETE'})
